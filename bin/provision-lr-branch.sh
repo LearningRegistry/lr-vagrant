01d4191bbfcf7a750726a3a73532dd563444809c
joe hobson <joe@navigationnorth.com>
2016-02-04 12:39:48 -0600

    temporary fix to provision script to get LR code pulling from Master

--- bin/provision-lr-branch.sh
+++ bin/provision-lr-branch.sh
@@ -21,12 +21,13 @@ url=$2
 tag=$3
 
 pushd /home/learnreg/LearningRegistry
-sudo -u learnreg git checkout master
-sudo -u learnreg git branch -D ${origin}/${tag}
-sudo -u learnreg git remote add ${origin} ${url}
+#sudo -u learnreg git checkout master
+#sudo -u learnreg git branch -D ${origin}/${tag}
+#sudo -u learnreg git remote add ${origin} ${url}
 sudo -u learnreg git fetch --all
-sudo -u learnreg git fetch --tags
-sudo -u learnreg git checkout tags/${tag} -b ${origin}/${tag}
+#sudo -u learnreg git fetch --tags
+#sudo -u learnreg git checkout tags/${tag} -b ${origin}/${tag}
+sudo -u learnreg git pull
 
 popd
 
